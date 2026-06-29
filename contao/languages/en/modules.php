787dbb850e31242fc17563629b99bfa67ddc56ae
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-29 11:01:09 +0330

    add english language file

---
+++ contao/languages/en/modules.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['MOD']['fooladgharb'] = 'Fooladgharb';
+$GLOBALS['TL_LANG']['MOD']['requests'] = ['Requests', 'Manage submitted requests.'];
--- contao/languages/en/tl_requests.php
+++ contao/languages/en/tl_requests.php
@@ -1,4 +1,29 @@
 <?php
 
+$GLOBALS['TL_LANG']['tl_requests']['name_legend'] = 'Contact information';
+$GLOBALS['TL_LANG']['tl_requests']['product_legend'] = 'Product information';
+$GLOBALS['TL_LANG']['tl_requests']['message_legend'] = 'Message';
+$GLOBALS['TL_LANG']['tl_requests']['datetime_legend'] = 'Date and source';
+
+$GLOBALS['TL_LANG']['tl_requests']['id'] = ['ID', 'Request ID'];
+$GLOBALS['TL_LANG']['tl_requests']['tstamp'] = ['Created', 'The date and time when the request was created.'];
+$GLOBALS['TL_LANG']['tl_requests']['name'] = ['Name', 'The requester name.'];
+$GLOBALS['TL_LANG']['tl_requests']['province'] = ['Province', 'The requester province.'];
+$GLOBALS['TL_LANG']['tl_requests']['phone'] = ['Phone', 'The requester phone number.'];
+$GLOBALS['TL_LANG']['tl_requests']['email'] = ['Email', 'The requester email address.'];
+$GLOBALS['TL_LANG']['tl_requests']['message'] = ['Message', 'The submitted message.'];
+$GLOBALS['TL_LANG']['tl_requests']['product'] = ['Product', 'The requested product.'];
+$GLOBALS['TL_LANG']['tl_requests']['type'] = ['Type', 'The requested product type.'];
+$GLOBALS['TL_LANG']['tl_requests']['usage'] = ['Usage', 'The intended usage.'];
+$GLOBALS['TL_LANG']['tl_requests']['insulation'] = ['Insulation', 'The selected insulation option.'];
+$GLOBALS['TL_LANG']['tl_requests']['area'] = ['Area', 'The requested area.'];
+$GLOBALS['TL_LANG']['tl_requests']['delivery'] = ['Delivery', 'The selected delivery option.'];
+$GLOBALS['TL_LANG']['tl_requests']['date'] = ['Date', 'The request date.'];
+$GLOBALS['TL_LANG']['tl_requests']['time'] = ['Time', 'The request time.'];
+$GLOBALS['TL_LANG']['tl_requests']['url'] = ['URL', 'The page URL where the request was submitted.'];
+$GLOBALS['TL_LANG']['tl_requests']['send_status'] = ['Send status', 'Whether the request has been sent to the external service.'];
+$GLOBALS['TL_LANG']['tl_requests']['lead_id'] = ['Lead ID', 'The external lead ID.'];
+$GLOBALS['TL_LANG']['tl_requests']['client_id'] = ['Client ID', 'The external client ID.'];
+
 $GLOBALS['TL_LANG']['tl_requests']['export_csv'] = ['Export CSV', 'Export all requests as a CSV file'];
 $GLOBALS['TL_LANG']['tl_requests']['export_excel'] = ['Export Excel', 'Export all requests as an Excel file'];
