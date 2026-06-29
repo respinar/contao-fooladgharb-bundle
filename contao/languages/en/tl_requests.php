f48e5495e55caf26d112578e228248424f4d84ae
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-29 10:33:17 +0330

    add translations

---
+++ contao/languages/en/tl_requests.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_requests']['export_csv'] = ['Export CSV', 'Export all requests as a CSV file'];
+$GLOBALS['TL_LANG']['tl_requests']['export_excel'] = ['Export Excel', 'Export all requests as an Excel file'];
