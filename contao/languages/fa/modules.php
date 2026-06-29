453f5e0bff866823bb13fa0eaaf9d16a9713a7e2
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-29 11:01:18 +0330

    add persian language file

---
+++ contao/languages/fa/modules.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['MOD']['fooladgharb'] = 'فولاد غرب';
+$GLOBALS['TL_LANG']['MOD']['requests'] = ['درخواست‌ها', 'مدیریت درخواست‌های ثبت‌شده.'];
---
+++ contao/languages/fa/tl_requests.php
@@ -0,0 +1,29 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_requests']['name_legend'] = 'اطلاعات تماس';
+$GLOBALS['TL_LANG']['tl_requests']['product_legend'] = 'اطلاعات محصول';
+$GLOBALS['TL_LANG']['tl_requests']['message_legend'] = 'پیام';
+$GLOBALS['TL_LANG']['tl_requests']['datetime_legend'] = 'تاریخ و منبع';
+
+$GLOBALS['TL_LANG']['tl_requests']['id'] = ['شناسه', 'شناسه درخواست'];
+$GLOBALS['TL_LANG']['tl_requests']['tstamp'] = ['تاریخ ایجاد', 'تاریخ و زمان ایجاد درخواست.'];
+$GLOBALS['TL_LANG']['tl_requests']['name'] = ['نام', 'نام ثبت‌کننده درخواست.'];
+$GLOBALS['TL_LANG']['tl_requests']['province'] = ['استان', 'استان ثبت‌کننده درخواست.'];
+$GLOBALS['TL_LANG']['tl_requests']['phone'] = ['تلفن', 'شماره تلفن ثبت‌کننده درخواست.'];
+$GLOBALS['TL_LANG']['tl_requests']['email'] = ['ایمیل', 'نشانی ایمیل ثبت‌کننده درخواست.'];
+$GLOBALS['TL_LANG']['tl_requests']['message'] = ['پیام', 'پیام ارسال‌شده.'];
+$GLOBALS['TL_LANG']['tl_requests']['product'] = ['محصول', 'محصول درخواستی.'];
+$GLOBALS['TL_LANG']['tl_requests']['type'] = ['نوع', 'نوع محصول درخواستی.'];
+$GLOBALS['TL_LANG']['tl_requests']['usage'] = ['کاربری', 'کاربری مورد نظر.'];
+$GLOBALS['TL_LANG']['tl_requests']['insulation'] = ['عایق', 'گزینه عایق انتخاب‌شده.'];
+$GLOBALS['TL_LANG']['tl_requests']['area'] = ['متراژ', 'متراژ درخواستی.'];
+$GLOBALS['TL_LANG']['tl_requests']['delivery'] = ['تحویل', 'گزینه تحویل انتخاب‌شده.'];
+$GLOBALS['TL_LANG']['tl_requests']['date'] = ['تاریخ', 'تاریخ درخواست.'];
+$GLOBALS['TL_LANG']['tl_requests']['time'] = ['زمان', 'زمان درخواست.'];
+$GLOBALS['TL_LANG']['tl_requests']['url'] = ['آدرس صفحه', 'آدرس صفحه‌ای که درخواست از آن ثبت شده است.'];
+$GLOBALS['TL_LANG']['tl_requests']['send_status'] = ['وضعیت ارسال', 'مشخص می‌کند درخواست به سرویس خارجی ارسال شده است یا نه.'];
+$GLOBALS['TL_LANG']['tl_requests']['lead_id'] = ['شناسه لید', 'شناسه لید در سرویس خارجی.'];
+$GLOBALS['TL_LANG']['tl_requests']['client_id'] = ['شناسه مشتری', 'شناسه مشتری در سرویس خارجی.'];
+
+$GLOBALS['TL_LANG']['tl_requests']['export_csv'] = ['خروجی CSV', 'دریافت همه درخواست‌ها در قالب فایل CSV'];
+$GLOBALS['TL_LANG']['tl_requests']['export_excel'] = ['خروجی اکسل', 'دریافت همه درخواست‌ها در قالب فایل اکسل'];
