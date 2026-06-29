fa66a5d12f3d7e20160fbc4c4796097e0df32633
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-29 09:45:51 +0330

    add config.php

---
+++ contao/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * This file is part of Fooladgharb Bundle.
+ *
+ * (c) Hamid Peywasti 2024 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
+$GLOBALS['BE_MOD']['fooladgharb']['requests']= [
+		'tables' => ['tl_requests']
+];
