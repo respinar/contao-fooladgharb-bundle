799f43697d88673f9d53b1c72e4bdb11a4cd6838
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-29 09:18:26 +0330

    add Plugin.php

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Whatsapp Button Bundle.
+ *
+ * (c) Hamid Peywasti 2025 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
+namespace Respinar\FooladgharbRequestBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Respinar\FooladgharbRequestBundle\FooladgharbRequestBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(FooladgharbRequestBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+}
