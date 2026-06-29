3004af757ec16de1aae449c2ff29d9001880ca2a
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-29 09:46:12 +0330

    add tl_form.php

---
+++ contao/dca/tl_form.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * This file is part of Fooladgharb Bundle.
+ *
+ * (c) Hamid Peywasti 2024 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
+$GLOBALS['TL_DCA']['tl_form']['fields']['targetTable']['options'][] = 'tl_requests';
