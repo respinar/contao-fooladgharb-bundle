9f2b579ee574636f76768f66b2b1765be809a8c0
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-09-07 22:14:23 +0330

    Convert language files from PHP to XLF

--- contao/languages/en/modules.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$GLOBALS['TL_LANG']['MOD']['fooladgharb'] = 'Fooladgharb';
-$GLOBALS['TL_LANG']['MOD']['requests'] = ['Requests', 'Manage submitted requests.'];
--- contao/languages/fa/modules.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$GLOBALS['TL_LANG']['MOD']['fooladgharb'] = 'فولاد غرب';
-$GLOBALS['TL_LANG']['MOD']['requests'] = ['درخواست‌ها', 'مدیریت درخواست‌های ثبت‌شده.'];
